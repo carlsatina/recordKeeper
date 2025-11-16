f9ad46ae5925fe32c5cf22e5dc2fb4b3005b5040
Roderick Carl Satina <roderick.satina@procept.com.au>
2025-11-16 08:50:28 +0800

    see all go to history.

--- frontend/src/views/mobile/MedicalRecords/Index.vue
+++ frontend/src/views/mobile/MedicalRecords/Index.vue
@@ -52,7 +52,7 @@
             <!-- Today's Reminder Section -->
             <div class="section-header">
                 <h3 class="section-title">Today's Reminder</h3>
-                <a href="#" class="see-all-link">See all</a>
+                <a href="#" class="see-all-link" @click="router.push('/medical-records/medicine-reminders/history')">See all</a>
             </div>
             <div class="reminder-list">
                 <div v-if="!hasActiveProfile" class="reminder-empty">

424e91d5593f1ecf9b0c314da771a9cf16c8fc79
Roderick Carl Satina <roderick.satina@procept.com.au>
2025-12-19 15:00:02 +0800

    Stop tracking frontend/backend in parent repo.

--- backend/extendedRequest.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express'
-
-interface ExtendedRequest extends Request {
-    user?: any
-    body: any
-}
-
-export { ExtendedRequest }
--- frontend/android/app/src/main/java/com/arshii/meclogger/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.arshii.meclogger;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-}
